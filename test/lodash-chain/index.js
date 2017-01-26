0bd1d8db7aeb842c2a0f9e7f3569ece9ac04ec13
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-26 16:43:41 -0500

    add example with lodash chain method

---
+++ test/lodash-chain/index.js
@@ -0,0 +1,13 @@
+var _ = require('lodash')
+var users = [
+  { 'user': 'barney',  'age': 36 },
+  { 'user': 'fred',    'age': 40 },
+  { 'user': 'pebbles', 'age': 1 }
+];
+
+var youngest = _
+  .chain(users)
+  .sortBy('age')
+  .head()
+  .value()
+console.log(youngest) //> {"user":"pebbles","age":1}
