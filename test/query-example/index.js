18d7d160760a3e9bb1df44bc657af7141c6d1f77
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-09 00:06:37 -0500

    playing with comment-value

--- test/query-example/index.js
+++ test/query-example/index.js
@@ -1,16 +1,19 @@
 // code example from
 // egghead.io/lessons/javascript-convert-a-querystring-to-an-object-using-function-composition-in-ramda
-const {identity, compose, fromPairs, map, split, tail} = require('ramda')
+// using tool https://github.com/bahmutov/comment-value
+const R = require('ramda')
 
 const queryString = '?page=2&pageSize=10&total=203'
 
-const parseQs = compose(
-  fromPairs,
-  map(split('=')), //> [["page","2"],["pageSize","10"],["total","203"]]
-  split('&'),      //> ["page=2","pageSize=10","total=203"]
-  tail             //> "page=2&pageSize=10&total=203"
+const parseQs = R.pipe(
+  R.tail,       //> "page=2&pageSize=10&total=203"
+  R.split('&'), //> ["page=2","pageSize=10","total=203"]
+  R.map(R.split('=')),
+  //> [["page","2"],["pageSize","10"],["total","203"]]
+  R.fromPairs
 )
 
 const result = parseQs(queryString)
 console.log(result)
 // result: {"page":"2","pageSize":"10","total":"203"}
+// target {"page":"2","pageSize":"10","total":"203"}
