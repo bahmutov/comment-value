708a6b6a1c6f57c35ef796c76025b56a11ec575f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-12 23:55:40 -0500

    example

--- test/query-example/index.js
+++ test/query-example/index.js
@@ -2,18 +2,17 @@
 // egghead.io/lessons/javascript-convert-a-querystring-to-an-object-using-function-composition-in-ramda
 // using tool https://github.com/bahmutov/comment-value
 const R = require('ramda')
-
-const queryString = '?page=2&pageSize=10&total=203'
+const queryString = '?page=2&pageSize=10&total=foo'
 
 const parseQs = R.pipe(
-  R.tail,       //> "page=2&pageSize=10&total=203"
-  R.split('&'), //> ["page=2","pageSize=10","total=203"]
+  R.tail,       //> "page=2&pageSize=10&total=foo"
+  R.split('&'), //> ["page=2","pageSize=10","total=foo"]
   R.map(R.split('=')),
-  //> [["page","2"],["pageSize","10"],["total","203"]]
+    //> [["page","2"],["pageSize","10"],["total","foo"]]
   R.fromPairs
 )
 
 const result = parseQs(queryString)
 console.log(result)
-// result: {"page":"2","pageSize":"10","total":"203"}
+// result: {"page":"2","pageSize":"10","total":"foo"}
 // target {"page":"2","pageSize":"10","total":"203"}
